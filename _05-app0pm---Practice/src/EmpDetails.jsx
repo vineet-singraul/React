783256b77fe7202ddfd545548750fcff31fd4b37
Vineet Singraul <singraulvineet943@gmail.com>
2025-02-10 14:50:41 +0530

    new code

---
+++ _05-app0pm---Practice/src/EmpDetails.jsx
@@ -0,0 +1,33 @@
+const EmpDetails = [
+    {
+      "EmpId": 101,
+      "Name": "Vineet",
+      "Post": "Developer",
+      "Salary": 20000,
+      "Joining Date": "01-06-2024"   
+    },
+    {
+      "EmpId": 102,
+      "Name": "Ananya",
+      "Post": "Designer",
+      "Salary": 25000,
+      "Joining Date": "15-05-2024"
+    },
+    {
+      "EmpId": 103,
+      "Name": "Rohan",
+      "Post": "Manager",
+      "Salary": 50000,
+      "Joining Date": "10-04-2023"
+    },
+    {
+      "EmpId": 104,
+      "Name": "Priya",
+      "Post": "HR",
+      "Salary": 30000,
+      "Joining Date": "20-03-2022"
+    }
+  ]
+
+
+  export default EmpDetails;
