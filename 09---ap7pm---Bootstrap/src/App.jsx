d08da4cfc03c72e85a4f8ee8f21c0899f016c5a7
Vineet Singraul <singraulvineet943@gmail.com>
2025-02-12 22:52:01 +0530

    new code

--- 09---ap7pm---Bootstrap/src/App.jsx
+++ 09---ap7pm---Bootstrap/src/App.jsx
@@ -64,6 +64,64 @@ const App = () => {
           <Button variant="primary">Go somewhere</Button>
         </Card.Body>
       </Card>
+
+
+      <Card style={{ width: '18rem' }}>
+        {/* Correctly referencing the imported img4 */}
+        <Card.Img variant="top" src={img4} />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+
+
+
+      <Card style={{ width: '18rem' }}>
+        {/* Correctly referencing the imported img4 */}
+        <Card.Img variant="top" src={img4} />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+
+
+
+      <Card style={{ width: '18rem' }}>
+        {/* Correctly referencing the imported img4 */}
+        <Card.Img variant="top" src={img4} />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+
+
+      <Card style={{ width: '18rem' }}>
+        {/* Correctly referencing the imported img4 */}
+        <Card.Img variant="top" src={img4} />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
     </>
   );
 }
