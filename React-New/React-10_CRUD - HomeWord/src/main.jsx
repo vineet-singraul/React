b3c45b384e3a6cffbadd312d846acc8caa08ed92
Vineet Singraul <singraulvineet943@gmail.com>
2025-04-13 22:17:31 +0530

    new code

--- React-New/React-10_CRUD - HomeWord/src/main.jsx
+++ React-New/React-10_CRUD - HomeWord/src/main.jsx
@@ -2,4 +2,5 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import "./style.css"
 import "bootstrap/dist/css/bootstrap.min.css";
+import 'react-toastify/dist/ReactToastify.css';
 createRoot(document.getElementById('root')).render(<App />)
