7d95dd6fc4d7f2c9c6b1fdaad25fc6937c24858e
Vineet Singraul <singraulvineet943@gmail.com>
2025-05-01 18:25:30 +0530

    new code

---
+++ React-New/iss.js
@@ -0,0 +1,12 @@
+let arr = [1, 2, 3, 99, 99, 66, 54,4];
+let indexToDelete = 2; 
+
+for (let i = indexToDelete; i < arr.length - 1; i++) {
+    arr[i] = arr[i + 1];
+arr.pop();
+
+}
+
+// arr.pop();
+
+console.log(arr);
