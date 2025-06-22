a2b17a0033a2a5b2294150b128e573bc03e4bb4d
Vineet Singraul <singraulvineet943@gmail.com>
2025-06-22 23:22:14 +0530

    new code

--- React-interview/Practice/R2-Props/componentProject/src/Cybrom.jsx
+++ React-interview/Practice/R2-Props/componentProject/src/Cybrom.jsx
@@ -2,7 +2,7 @@ const Cybrom = ({Greet}) => {
   return (
     <>
        <h1>Cybrom : {Greet}</h1>
-       <button onClick={()=>{Greet()}}>Click Me !</button>
+       <button onClick={()=>{Greet()}}>Click Me ! hare</button>
     </>
   )
 }
